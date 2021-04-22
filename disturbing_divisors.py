901f58a0e8a701a42ac76b68c0a0cb3d3ae89124
Shervin <69309473+shervin-h@users.noreply.github.com>
2021-04-22 21:51:57 +0430

    Update disturbing_divisors.py

--- disturbing_divisors.py
+++ disturbing_divisors.py
@@ -1,16 +1,15 @@
 n = input()
 n = n.strip()
 
-d = dict()
 sum = 0
+count = 0
+
 for i in range(1, int(n)+1):
-    d[i] = list()
     for j in range(1, i+1):
         if i % j == 0:
-            d[i].append(j)
             sum += j
+            count += 1
 
-
-print(len(d), sum)
+print(count, sum)
 
 # by Shervin Hasanzadeh
