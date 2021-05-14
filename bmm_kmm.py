b1716632a7b24084e8d173e5262238e7acc38f4d
Shervin <69309473+shervin-h@users.noreply.github.com>
2021-05-14 18:12:44 -0400

    Create bmm_kmm.py

---
+++ bmm_kmm.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().strip().split())
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+print(gcd(n, m), lcm(n, m))
