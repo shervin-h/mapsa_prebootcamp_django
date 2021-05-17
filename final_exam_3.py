97c51e9ba62d8866aa88f9c9bcf519dc045eab5e
Shervin <69309473+shervin-h@users.noreply.github.com>
2021-05-17 16:53:30 -0400

    Update final_exam_3.py

--- final_exam_3.py
+++ final_exam_3.py
@@ -25,6 +25,39 @@ Student_submissions = {
     ],
 }
 
+result = list()
+for student in Student_submissions.items():
+    # print(student)
+    d = dict()
+    d['name'] = student[0]
+    d['correct'] = 0
+    d['wrong'] = 0
+    j = 0
+    for v in student[1]:
+        # print(v)
+        if list(v.items())[0][1] == correct_answers[j][1]:
+            d['correct'] += 1
+            j += 1
+        else:
+            d['wrong'] += 1
+            j += 1
+        # print(j)
+    result.append(d)
+
+# print(result)
+
+res = dict()
+for student in result:
+    # print(student)
+    percent = ((3*student['correct']) - student['wrong']) / ((student['correct'] + student['wrong']) * 3)
+    res[student['name']] = percent
+
+
+f = open('names_marks.txt', 'w', encoding='UTF-8')
+# f.write(json.dump(res))
+# or
+f.write(str(res))
+f.close()
 
 
 #by Shervin Hasanzadeh
